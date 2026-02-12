5dacda85e97800d0ddaf82ef1b468680f2b97bf0
T Harshavardhan <148187267+harshaterala@users.noreply.github.com>
2026-02-12 13:02:03 +0530

    Update App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Dashboard from './pages/Dashboard';
+import './styles/app.css';
+
+function App() {
+  return (
+    <div className="app">
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
